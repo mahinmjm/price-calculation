d03dccbececb76ed43463a68c9da73e086c24336
mahin <mahinmjm@gmail.com>
2020-05-21 14:24:02 +0600

    Update app.js

--- app.js
+++ app.js
@@ -3,18 +3,19 @@
 axios.get('https://test-schema.herokuapp.com/vegetables')
   .then(function (response) {  
     data = response.data.data; 
-    console.log(data);
+    //console.log(data);
     
 
     let total = data.reduce( (total, item) =>  total += item.price, 0 ); 
-    console.log(total);
+    console.log("Total: "+ total);
 
 
     const vat15 = data.map(item => item.price + parseFloat(item.price) * .15 );
-
+    console.log("Apply 15% Vat to All Price" );
     console.log(vat15);
     
     const morethan50 = data.filter(item => parseInt(item.price) > 50  == 1);
+    console.log("Filter which price are more than 50" );
     console.log(morethan50); 
 
   })
